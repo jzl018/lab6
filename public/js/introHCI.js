4bfca048aeb568ff0f42260f0394487845538ae7
jzl018@ucsd.edu <jzl018@ucsd.edu>
2017-02-16 11:50:55 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,20 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+	var projectHTML = '<img src="' + result['image'] + 
+					'" class ="detailsImage">' + '<p>' + 
+					result['title'] + '</p>' + '<p><small>' 
+					+ result['date'] + '</small></p>' +
+					result['summary'];
+	var projectid = '#project' + result['id'];
+	$(projectid + " .details").html(projectHTML);
+	console.log(result);
 }
 
 /*
@@ -34,5 +47,14 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get('/palette', function(result){
+		console.log(result)
+		var colors = result.colors.hex;
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	})
 	console.log("User clicked on color button");
 }
